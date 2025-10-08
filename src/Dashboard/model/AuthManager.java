7b94ad06c632b1db960bd9f342b87dfd05b06ba5
Bless <tetaangelbless014@gmail.com>
2025-10-08 22:36:06 +0200

    Initial commit

--- src/Dashboard/model/AuthManager.java
+++ src/Dashboard/model/AuthManager.java
@@ -1,7 +1,12 @@
 package Dashboard.model;
 
+import java.util.ArrayList;
+
 public class AuthManager {
+    private ArrayList<Account> accounts;
+    private final String fileName ="accounts.dat";
     public void signUp(String userName,String password,Student student){
+
     }
 
 
