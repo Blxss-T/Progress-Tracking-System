a7152c1499530d7876faeff15219a08d225726d6
Bless <tetaangelbless014@gmail.com>
2025-10-08 21:45:30 +0200

    Initial commit

--- src/Dashboard/model/Account.java
+++ src/Dashboard/model/Account.java
@@ -4,5 +4,11 @@ public class Account {
     private String userName;
     private String passwordHash;
     private Student student;
+    public Account (){}
+    public Account (String name,String pw){
+        this.userName=name;
+        this.passwordHash=pw;
+
+    }
 
 }
