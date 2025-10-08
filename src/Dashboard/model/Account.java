a12b90490665706c7c362632cfea0ead611b8f61
Bless <tetaangelbless014@gmail.com>
2025-10-08 21:46:36 +0200

    Initial commit

--- src/Dashboard/model/Account.java
+++ src/Dashboard/model/Account.java
@@ -8,7 +8,29 @@ public class Account {
     public Account (String name,String pw){
         this.userName=name;
         this.passwordHash=pw;
-
     }
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
