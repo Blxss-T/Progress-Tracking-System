a2f54b648f60336341bf86c36e25aca562a32b91
Bless <tetaangelbless014@gmail.com>
2025-10-19 21:56:47 +0200

    Initial commit

--- src/Dashboard/model/Account.java
+++ src/Dashboard/model/Account.java
@@ -40,5 +40,6 @@ public class Account {
                 "userName='" + userName + '\'' +
                 ", passwordHash='" + passwordHash + '\'' +
                 ", student=" + student +
+                '}';
     }
 }
