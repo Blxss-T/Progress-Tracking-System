61ea0dab78554b4f190c554d5557c75a8bbb86c1
Bless <tetaangelbless014@gmail.com>
2025-10-22 19:10:02 +0200

    initial commit

--- src/Dashboard/UserAuth/passwordsUtil.java
+++ src/Dashboard/UserAuth/passwordsUtil.java
@@ -1,10 +1,11 @@
 package Dashboard.UserAuth;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
-import javax.security.;
+import java.security.SecureRandom;
 import java.util.Base64;
 
-
 public class passwordsUtil {
+    SecureRandom sr= new SecureRandom();
+    byte[] salt= new byte[]
 
 }
