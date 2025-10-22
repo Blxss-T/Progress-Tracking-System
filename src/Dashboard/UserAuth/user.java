a53d6143310fe61e03741d47e7942c51b5beb112
Bless <tetaangelbless014@gmail.com>
2025-10-22 18:19:40 +0200

    initial commit

--- src/Dashboard/UserAuth/user.java
+++ src/Dashboard/UserAuth/user.java
@@ -29,4 +29,12 @@ public user(){}
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
